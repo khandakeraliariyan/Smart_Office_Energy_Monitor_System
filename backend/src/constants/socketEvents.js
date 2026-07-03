33c725b912d0d5020d28f1cf282145d9df48d4fd
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-04 00:02:25 +0600

    added the socket events

---
+++ backend/src/constants/socketEvents.js
@@ -0,0 +1,12 @@
+const SOCKET_EVENTS = {
+    CONNECTION: "connection",
+    DISCONNECT: "disconnect",
+
+    DEVICE_UPDATED: "deviceUpdated",
+    POWER_UPDATED: "powerUpdated",
+    ALERT_UPDATED: "alertUpdated",
+
+    DASHBOARD_UPDATED: "dashboardUpdated",
+};
+
+module.exports = SOCKET_EVENTS;
