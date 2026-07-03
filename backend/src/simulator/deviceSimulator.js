21053dc92f9669f661d6de8b4efac319e4807430
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-03 21:20:42 +0600

    added the simulator for device

---
+++ backend/src/simulator/deviceSimulator.js
@@ -0,0 +1,54 @@
+const Device = require("../models/Device");
+const deviceService = require("../services/device.service");
+
+const startSimulator = async (io) => {
+
+    console.log("🚀 Device Simulator Started");
+
+    setInterval(async () => {
+
+        try {
+
+            const devices = await deviceService.getAllDevices();
+
+            if (!devices.length) return;
+
+            const randomDevice =
+                devices[Math.floor(Math.random() * devices.length)];
+
+            const newStatus = !randomDevice.status;
+
+            const updatedDevice =
+                await deviceService.updateDevice(randomDevice._id, {
+
+                    status: newStatus,
+
+                    currentPower: newStatus
+                        ? randomDevice.powerRating
+                        : 0,
+
+                    lastChanged: new Date()
+
+                });
+
+            console.log(
+                `${updatedDevice.name} -> ${updatedDevice.status ? "ON" : "OFF"}`
+            );
+
+            if (io) {
+
+                io.emit("deviceUpdated", updatedDevice);
+
+            }
+
+        } catch (err) {
+
+            console.log(err);
+
+        }
+
+    }, 5000);
+
+};
+
+module.exports = startSimulator;
