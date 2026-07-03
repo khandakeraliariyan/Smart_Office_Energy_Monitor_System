02c2b10fb12feae9cdf42f67663d8a55e341514d
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-03 22:07:12 +0600

    created the room service function

---
+++ backend/src/services/room.service.js
@@ -0,0 +1,40 @@
+const Room = require("../models/Room");
+const Device = require("../models/Device");
+
+class RoomService {
+
+    async getAllRooms() {
+
+        const rooms = await Room.find();
+
+        const roomData = await Promise.all(
+
+            rooms.map(async (room) => {
+
+                const devices = await Device.find({
+                    room: room._id
+                });
+
+                const totalPower = devices.reduce(
+                    (sum, device) =>
+                        sum + (device.status ? device.powerRating : 0),
+                    0
+                );
+
+                return {
+                    ...room.toObject(),
+                    devices,
+                    totalPower
+                };
+
+            })
+
+        );
+
+        return roomData;
+
+    }
+
+}
+
+module.exports = new RoomService();
