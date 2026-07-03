f5b5277d593f7f167cfe1a160004cb2455daeec8
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-03 20:48:27 +0600

    added the initial code for app and server

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(compression());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Smart Office Monitoring API Running"
+    });
+});
+
+module.exports = app;
